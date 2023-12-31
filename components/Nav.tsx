4d8f10933b3cc6716532aae726c453a2c164fc8a
Arta <arta@Artas-MacBook-Air.local>
2023-12-31 23:34:22 +0330

    [feat] added back button, added text for desktop, added github in navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,28 @@
+import { useRouter } from "next/router";
 import React from "react";
-import { SiYoutubeshorts } from "react-icons/si";
+import { SiYoutubeshorts, SiGithub } from "react-icons/si";
+import { BsArrowLeft } from "react-icons/bs";
 
 const Nav = () => {
+  const router = useRouter();
   return (
     <div className="w-full h-12 hstack justify-between px-5 text-rose-500">
-      <h3 className="pt-2">YTB</h3>
-      <SiYoutubeshorts className="text-2xl" />
+      <div className="flex items-center">
+        <h3 className="pt-2 font-bold">YTB</h3>
+        <a
+          href="https://github.com/sabber-slt/youtube"
+          target="_blank"
+          title="YTB on Github"
+        >
+          <SiGithub className="text-2xl ms-6 text-gray-400 hover:text-gray-200" />
+        </a>
+      </div>
+      <div className="flex items-center">
+        <SiYoutubeshorts className="text-2xl" />
+        {router.asPath.includes("video") && (
+          <BsArrowLeft className="text-2xl ms-6 text-gray-400" onClick={() => router.back()} />
+        )}
+      </div>
     </div>
   );
 };
